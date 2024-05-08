bbfbf77a25241961f35a9b3b5e984d9aa5b9c6b3
Imad Jhabli <imadjhabli@gmail.com>
2024-05-09 00:08:51 +0100

    done

--- 0x01-react_intro/task_3/dashboard/src/setupTests.js
+++ 0x01-react_intro/task_3/dashboard/src/setupTests.js
@@ -1,7 +1,4 @@
+import { configure } from 'enzyme';
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
 
-// task_3/dashboard/src/setupTests.js
-
-import Enzyme from 'enzyme';
-import Adapter from 'enzyme-adapter-react-17-updated';
-
-Enzyme.configure({ adapter: new Adapter() });
+configure({ adapter: new Adapter() });
